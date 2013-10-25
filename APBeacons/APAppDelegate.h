e56e390d19aebaefbe483b6d7b014aa961fb40da
ashutosh <ashutosh@eemelabs.com>
2013-10-25 01:21:41 -0700

    Initial Commit

---
+++ APBeacons/APAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APAppDelegate.h
+//  APBeacons
+//
+//  Created by Ashutosh Priyadarshy on 10/25/13.
+//  Copyright (c) 2013 EEMe labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ APBeaconsTests/APBeaconsTests.m
@@ -0,0 +1,34 @@
+//
+//  APBeaconsTests.m
+//  APBeaconsTests
+//
+//  Created by Ashutosh Priyadarshy on 10/25/13.
+//  Copyright (c) 2013 EEMe labs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface APBeaconsTests : XCTestCase
+
+@end
+
+@implementation APBeaconsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
